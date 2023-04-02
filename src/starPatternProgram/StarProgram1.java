f271d6e16fc32def7c4efde0c988ef7c4a8c5ccb
BusinessComputers.in <BusinessComputers.in@DESKTOP-NUJTOEE>
2023-04-02 22:05:45 +0530

    arbaj

---
+++ src/starPatternProgram/StarProgram1.java
@@ -0,0 +1,51 @@
+package starPatternProgram;
+
+public class StarProgram1 {
+	public static void main(String[] args) 
+{
+//		 for(int i=1;i<=5; i++)
+//		    {
+//		    	for(int j=1; j<=5; j++)
+//		    	{
+//		    	System.out.print("* ");
+//		    }
+//		    	
+//			System.out.println();
+//			
+//	}
+
+//		 for(int a=1;a<=5;a++)
+//		 {
+//			 for(int b = 1; b<=5; b++)
+//			 {
+//				 System.out.print("arbaj  ");
+//			 }
+//		 
+//		 System.out.println();
+//		 
+//		 }	 
+		 
+		 for (int i=1; i<=5; i++)
+		 {
+			for(int j=1;j<=i;j++)
+			 System.out.print("* " );
+		 }
+		 
+		 System.out.println();
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+}
+}
